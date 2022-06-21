149fd966c7f06a460920b01ef46100e678322205
candradwicahyo <candradwicahyo18@gmail.com>
2022-06-21 10:25:19 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+## Check Last Index
+
+sebuah program dengan nama check last index yang dibuat dengan bahasa pemrograman javascript. dimana program ini berguna untuk mengecek index terakhir dari sebuah string / text yang diketik
+
+### dibuat dengan berikut
+
+* HTML
+* CSS
+  * [Bootstrap](https://grtbootstrap.com)
+* Javascript
+
+### lainnya
+
+* [SweetAlert2](https://sweetalert2.github.io)
+* handphone
+* code editor (Acode)
+* edit Gambar (Pixellan)
+
+### preview
+
+![result](https://github.com/candradwicahyo/check-last-index/blob/master/image.jpg)
+
+[tekan link ini untuk membuat program ini](https://candradwicahyo.github.io/check-last-index)
+
+> dibuat oleh **candra dwi cahyo**
---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', () => {
+  
+  const input = document.querySelector('.text');
+  const button = document.querySelector('.submit');
+  button.addEventListener('click', () => {
+    const value = input.value.trim();
+    if (!value) return sweetalert('error', 'error', 'input masih kosong!');
+    const text = message(value);
+    sweetalert('success', 'berhasil', text);
+    clear();
+  });
+  
+  function sweetalert(icon, title, text) {
+    return swal.fire ({
+      icon: icon,
+      title: title,
+      text: text
+    });
+  }
+  
+  function clear() {
+    input.value = '';
+  }
+  
+  function message(value) {
+    const lastIndex = value.length - 1;
+    return `panjang karakter dari text yang anda tulis adalah ${value.length} dan index terakhir dari text yang anda tulis adalah ${lastIndex}. kenapa bisa begitu? dikarenakan index pertama selalu berawal dari angka 0`;
+  }
+  
+});
